a020093b534734cee54458514b4752a8c8946369
bamboo321 <28136882+bamboo321@users.noreply.github.com>
2018-09-01 04:29:30 +0900

    test

---
+++ libpcap.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pcap/pcap.h>
+
+
+void printPcap_if(pcap_if_t *a) {
+    printf("next: %p\n", a->next);
+    printf("name: %s\n", a->name);
+    printf("description: %s\n", a->description);
+    printf("address: %p\n", a->addresses);
+    printf("flags: %d\n", a->flags);
+    printf("-------------\n");
+}
+
+int main() {
+    char pcap_errbuf[PCAP_ERRBUF_SIZE];
+    pcap_errbuf[0] = '\0';
+
+    //
+    //find wireless device
+    //
+    pcap_if_t *alldevsp;
+    if (pcap_findalldevs(&alldevsp, pcap_errbuf) == -1) {
+        printf("%s\n", pcap_errbuf);
+        exit(EXIT_FAILURE);
+    }
+
+
+    pcap_if_t *currentAddress = alldevsp;
+    while (currentAddress->next != NULL) {
+        printPcap_if(currentAddress);
+        currentAddress = currentAddress->next;
+    }
+
+
+
+
+
+    pcap_t *pcap = pcap_open_live("eth0", 96, 0, 0, pcap_errbuf);
+
+    if (pcap_errbuf[0] != '\0') {
+        fprintf(stderr, "%s", pcap_errbuf);
+    }
+
+    if (!pcap) {
+        exit(1);
+    }
+}
+
